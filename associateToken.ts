280ce16e177d76755b02cf43542eadc3ec35c8cd
SilverEskimo <slavasereb@gmail.com>
2023-05-09 19:13:19 +0300

    Remove get deposit address

--- associateToken.ts
+++ associateToken.ts
@@ -11,17 +11,20 @@ const network: string = 'testnet';
 
 // Set token ID, for example "0.0.4577248"
 const TOKEN_ID: string = "<token_id>"
+
+// Fireblocks API auth artifacts
 const privkeyPath: string = "<path_to_private_key_file>";
 const apiKey: string = "<api_key>";
 
+
 // Add your vault account IDs to the array, for example [0, 1, 2]
-const vaultAccountIds: number[] = []
+const vaultAccountIds: number[] = [  ]
 
 
 const associateToken = async() => {
 
     try {
-
+    
         const apiSecret = fs.readFileSync(path.resolve(__dirname, privkeyPath), "utf8");
         const fireblocks = new FireblocksSDK(apiSecret, apiKey);
     
@@ -29,15 +32,14 @@ const associateToken = async() => {
         
             const provider = new FireblocksProvider(fireblocks, vaultId, network);
             const client = await provider.getClient()
-            const accoundId = (await fireblocks.getDepositAddresses(String(vaultId), "HBAR_TEST"))[0].address
-
+            
             const transaction = await new TokenAssociateTransaction()
-            .setAccountId(accoundId)
+            .setAccountId(provider.address)
             .setTokenIds([TOKEN_ID])
             .setNodeAccountIds([new AccountId(3)])
             .execute(client)
             .catch( e => {
-                console.log("Submitting to the network failed:\n", e)
+                console.log("Submission to the network failed:\n", e)
             })
             
             console.log(transaction)
